07f09d9471c1f7d77cbb4e72c8fa5ca55cb39a4c
irenaeus1 <sweethim38@gmail.com>
2025-02-05 19:24:27 +0900

    feat: solve BOJ problems - boj_10171

---
+++ 250205/boj_10171.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(nullptr);
+
+    std::cout << "\\    /\\" << std::endl;
+    std::cout << " )  ( ')" << std::endl;
+    std::cout << "(  /  )" << std::endl;
+    std::cout << " \\(__)|" << std::endl;
+    return 0;
+}
